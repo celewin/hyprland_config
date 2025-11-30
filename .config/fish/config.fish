11d804a70552b01a84c249ae97e0ed111c10d60f
celewin <c.lewinske@gmail.com>
2025-11-30 15:51:37 +0100

    new wallpapers

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 alias show='kitty +kitten icat'
+alias feh='feh -g 640x360 --scale-down'
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
