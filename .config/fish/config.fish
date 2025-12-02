1caacbd5ec8d66894130dcc0e12868f2a124037f
celewin <c.lewinske@gmail.com>
2025-12-02 22:37:23 +0100

    kitty ssh fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 alias show='kitty +kitten icat'
 alias feh='feh -g 640x360 --scale-down'
+set -gx TERM xterm-256color
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
